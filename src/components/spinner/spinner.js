a0b74e9f1fd3a027af8b981fabbecf21cc15b60f
Anton Teplyakov <teplyakoffadns@yandex.ru>
2020-07-06 10:46:38 +1000

    start changes, single page application with StarWars Api

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './spinner.css';
+
+const Spinner = () =>{
+    return(
+        <div className="loadingio-spinner-double-ring-vddji7igclj">
+            <div className="ldio-19u3l9nn0a1">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
